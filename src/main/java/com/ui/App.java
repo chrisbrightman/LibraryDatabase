92feb20303011dd8f4fe1ade1a915b9ec20ec20e
chris <chris5brigtman@gmail.com>
2020-12-22 11:22:51 -0500

    trying to get javafx to work

---
+++ src/main/java/com/ui/App.java
@@ -0,0 +1,13 @@
+package com.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public void start(  Stage primaryStage) {}
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        launch(args);
+    }
+}
