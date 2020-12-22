f844b8984164b97543a44644e97e0d5592ec944d
chris <chris5brigtman@gmail.com>
2020-12-22 12:45:46 -0500

    added JDBC to gradle project

--- src/main/java/com/ui/App.java
+++ src/main/java/com/ui/App.java
@@ -1,10 +1,30 @@
 package com.ui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public void start(  Stage primaryStage) {}
+    public void start(  Stage primaryStage) {
+        StackPane root = new StackPane(new Label("Hello World!"));
+
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Should not happen!");
+            }
+
+            System.exit(0);
+        }).start();
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
